5938be0aa4b2168e68ef1f1d01908d595ceec47d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "system_center_virtual_machine_manager_virtual_networks" {
-  description = "All system_center_virtual_machine_manager_virtual_network resources"
-  value       = azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks
-}
 output "system_center_virtual_machine_manager_virtual_networks_custom_location_id" {
-  description = "List of custom_location_id values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.custom_location_id]
+  description = "Map of custom_location_id values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.custom_location_id }
 }
 output "system_center_virtual_machine_manager_virtual_networks_location" {
-  description = "List of location values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.location]
+  description = "Map of location values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.location }
 }
 output "system_center_virtual_machine_manager_virtual_networks_name" {
-  description = "List of name values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.name]
+  description = "Map of name values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.name }
 }
 output "system_center_virtual_machine_manager_virtual_networks_resource_group_name" {
-  description = "List of resource_group_name values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.resource_group_name]
+  description = "Map of resource_group_name values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.resource_group_name }
 }
 output "system_center_virtual_machine_manager_virtual_networks_system_center_virtual_machine_manager_server_inventory_item_id" {
-  description = "List of system_center_virtual_machine_manager_server_inventory_item_id values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.system_center_virtual_machine_manager_server_inventory_item_id]
+  description = "Map of system_center_virtual_machine_manager_server_inventory_item_id values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.system_center_virtual_machine_manager_server_inventory_item_id }
 }
 output "system_center_virtual_machine_manager_virtual_networks_tags" {
-  description = "List of tags values across all system_center_virtual_machine_manager_virtual_networks"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : v.tags]
+  description = "Map of tags values across all system_center_virtual_machine_manager_virtual_networks, keyed the same as var.system_center_virtual_machine_manager_virtual_networks"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_network.system_center_virtual_machine_manager_virtual_networks : k => v.tags }
 }
 
